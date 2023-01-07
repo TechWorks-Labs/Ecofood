c664240f6eb911345dcef3ffc58751c47620fc59
gibewevi <gibewevi@skiff.com>
2023-01-07 19:17:01 +0100

    header toggle

---
+++ frontend/src/containers/Site.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './homePage/HomePage';
+
+class Site extends Component {
+    render() {
+        return (
+            <div className="">
+                <Routes>
+                    <Route path="/homepage" exact element={<HomePage />} />
+                    <Route path="*" element={<span>ERROR 404</span>} />   
+                </Routes>
+            </div>
+        )
+    }
+}
+
+export default Site;
