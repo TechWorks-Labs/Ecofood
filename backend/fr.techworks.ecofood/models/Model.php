da709e8719910160edcd356db715ecec7a9308d4
gibewevi <gibewevi@skiff.com>
2023-01-07 21:23:47 +0100

    update model

---
+++ backend/fr.techworks.ecofood/models/Model.php
@@ -0,0 +1,29 @@
+<?php
+abstract class Model {
+    private static $pdo;
+
+    private static function setBdd(){
+        $user = $_ENV["DB_USER"];
+        $password = $_ENV["DB_PASSWORD"];
+        $host = $_ENV["DB_HOST"];
+        $database = $_ENV["DB_DATABASE"];
+
+        self::$pdo = new PDO("mysql:host=$host;port=9501;dbname=$database;charset=utf8",$user,$password);
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    protected function getBdd(){
+        if( self::$pdo === null ){
+            self::setBdd();
+        }
+        var_dump(self::$pdo);
+        
+        return self::$pdo;
+    }
+
+    public static function sendJSON($info){
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json");
+        echo json_encode($info);
+    }
+}
