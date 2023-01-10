1c9c4e13b10f3abce9de1e1ef1835f4e61a25532
VicLam <tanacetifolia@gmail.com>
2023-01-10 15:19:30 +0100

    refacto

--- backend/fr.techworks.ecofood/index.php
+++ backend/fr.techworks.ecofood/index.php
@@ -2,9 +2,6 @@
 
 use Autoload\Autoloader;
 use Controllers\Api\ApiController;
-use Models\Model;
-
-var_dump($_ENV);
 
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('DS', DIRECTORY_SEPARATOR);
@@ -12,36 +9,30 @@ define('DS', DIRECTORY_SEPARATOR);
 require('Autoload/Autoloader.php');
 Autoloader::register();
 
+$api_controller = new ApiController();
 
-$bdd = Model::getBdd();
-var_dump($bdd);
+$url = explode('?', $_SERVER['REQUEST_URI'])[0];
+$data = explode('/', $url);
+$page = $data[1];
 
-// $api_controller = new ApiController();
+try{
+    if(empty($_GET['page'])) {
+        throw new Exception ("url doesn't exist");
+    } else {
+        $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
+        switch($url[0]){
+            case "product" :
+                switch($url[1]){
+                    case "fruit": $api_controller->getProductByType(1);
+                    break;
+                    case "vegetable": $api_controller->getProductByType(2);
+                    break;
+                    case "meat": $api_controller->getProductByType(3);
+                    break;
+                    default: throw new Exception ("url doesn't exist"); 
+                }
+        }
+    }
+} catch (Exception $e) {
 
-// $url = explode('?', $_SERVER['REQUEST_URI'])[0];
-// $data = explode('/', $url);
-// $page = $data[1];
-
-// var_dump($_ENV);
-
-// try{
-//     if(empty($_GET['page'])) {
-//         throw new Exception ("url doesn't exist");
-//     } else {
-//         $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
-//         switch($url[0]){
-//             case "product" :
-//                 switch($url[1]){
-//                     case "fruit": $api_controller->getProductByType(1);
-//                     break;
-//                     case "vegetable": $api_controller->getProductByType(2);
-//                     break;
-//                     case "meat": $api_controller->getProductByType(3);
-//                     break;
-//                     default: throw new Exception ("url doesn't exist"); 
-//                 }
-//         }
-//     }
-// } catch (Exception $e) {
-
-// }
+}
